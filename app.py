b9da58560f84776f15c503d769672c7a14528929
usertina <agustinacalleja@gmail.com>
2024-05-13 14:21:48 +0200

    añadido formulario

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -23,34 +23,28 @@ def index():
     conn.close()
     return render_template('index.html', users=users)
 
-@app.route('/first_name')
-def index_first_name():
-    conn = get_db_connection()
-    users = conn.execute('SELECT first_name FROM users').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        age = request.form['age']
+        phone = request.form['phone']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO users (first_name, last_name, age, phone) VALUES (?, ?, ?, ?)',
+                     (first_name, last_name, age, phone))
+        conn.commit()
+        conn.close()
+    return redirect('/')
 
-@app.route('/age')
-def index_age():
-    conn = get_db_connection()
-    users = conn.execute('SELECT age FROM users').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
-
-@app.route('/last_name')
-def index_last_name():
-    conn = get_db_connection()
-    users = conn.execute('SELECT last_name FROM users').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
-
-@app.route('/phone')
-def index_phone():
-    conn = get_db_connection()
-    users = conn.execute('SELECT phone FROM users').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
-
+@app.route('/delete_user/<int:id>', methods=['POST'])
+def delete_user(id):
+    if request.method == 'POST':
+        conn = get_db_connection()
+        conn.execute('DELETE FROM users WHERE id = ?', (id,))
+        conn.commit()
+        conn.close()
+    return redirect('/')
 
 if __name__ == '__main__':
     init_db()
